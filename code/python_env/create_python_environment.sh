fbb0ed8d8070d679b77a08e5661d3ef459b0e557
Golia Shafiei <golia.shafiei@pennmedicine.upenn.edu>
2026-02-16 09:33:07 -0500

    adds python env yml

---
+++ code/python_env/create_python_environment.sh
@@ -0,0 +1,6 @@
+# setting up the Conda Environment
+# to create the `timescale_env` environment, run:
+
+cd /cbica/projects/developmental_gradients/gitrepo/shafiei_timescale/code/python_env
+conda env create -f timescale_env.yml
+conda activate timescale_env
